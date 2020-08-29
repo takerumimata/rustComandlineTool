d84ef08cb54bdc25cf9315cc7bb21faf91b23845
takerumimata <fossil1908@gmail.com>
2020-08-29 14:32:36 +0900

    hello rust

---
+++ target/debug/deps/samplecli.d
@@ -0,0 +1,5 @@
+/Users/takeru/Documents/Software_Engineering/rust/rustComandlineTool/target/debug/deps/samplecli: src/main.rs
+
+/Users/takeru/Documents/Software_Engineering/rust/rustComandlineTool/target/debug/deps/samplecli.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/samplecli.d
@@ -0,0 +1,1 @@
+/Users/takeru/Documents/Software_Engineering/rust/rustComandlineTool/target/debug/samplecli: /Users/takeru/Documents/Software_Engineering/rust/rustComandlineTool/src/main.rs
